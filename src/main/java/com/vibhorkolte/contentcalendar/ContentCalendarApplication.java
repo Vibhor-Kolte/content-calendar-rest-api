21f6c66e78d693a4a036a7a3312bf37f17297610
Vibhor-Kolte <47689130+Vibhor-Kolte@users.noreply.github.com>
2024-01-04 23:21:00 +0530

    [Release-001] - Get Content (#2)
    * [DV-001] - Get all the beans in the IOC Container
    
    * [DV-001] - Create Content Record
    
    * [DV-001] - Create Content Rest Controller
    
    * [DV-001] - Create Content Repository with findAll and findById Methods
    
    * [DV-001] - Inversion of Control Demonstration
    
    * [DV-001] - configure api endpoint for findAll method
    
    * [DV-001] - initialize the repository using @PostConstruct
    
    * [DV-001] - configure findById rest endpoint
    
    * [DV-001] - handle if content not found

--- src/main/java/com/vibhorkolte/contentcalendar/ContentCalendarApplication.java
+++ src/main/java/com/vibhorkolte/contentcalendar/ContentCalendarApplication.java
@@ -1,13 +1,22 @@
 package com.vibhorkolte.contentcalendar;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ContentCalendarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContentCalendarApplication.class, args);
+		ConfigurableApplicationContext context =  SpringApplication.run(ContentCalendarApplication.class, args);
+		// Get all the beans
+//		context.getBeanDefinitionNames();
+//		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		
+		// Get a single bean by name
+		System.out.println(context.getBean("contentCalendarApplication"));
 	}
 
 }
---
+++ src/main/java/com/vibhorkolte/contentcalendar/controller/ContentController.java
@@ -0,0 +1,48 @@
+package com.vibhorkolte.contentcalendar.controller;
+
+import com.vibhorkolte.contentcalendar.model.Content;
+import com.vibhorkolte.contentcalendar.repository.ContentCollectionRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/content")
+public class ContentController {
+	
+	private final ContentCollectionRepository repository;
+	
+//	// Manually Creating Dependency Object
+//	public ContentController() {
+//		repository = new ContentCollectionRepository();
+//	}
+	
+	// Ask Spring to give object from its IOC Container..{Constructor Injection}
+	// @Autowired by default when you have only one public constructor (no need to specify explicitly)
+	public ContentController(ContentCollectionRepository repository) {
+		this.repository = repository;
+	}
+	
+	// Make a request and find all content in the system
+	@GetMapping("")
+	public List<Content> findAll(){
+		return repository.findAll();
+	}
+	
+	// Make a request and find a content by ID.
+	@GetMapping("/{id}")	// {} dynamic placeholder
+	public Content findById(@PathVariable Integer id){
+		return repository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found"));
+	}
+	
+
+}
