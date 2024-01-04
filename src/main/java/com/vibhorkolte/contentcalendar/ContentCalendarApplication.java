37d9cdc49c99a55224936e0e1c83abd8b902362f
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-01-04 22:25:20 +0530

    [DV-001] - Create Content Record

--- src/main/java/com/vibhorkolte/contentcalendar/ContentCalendarApplication.java
+++ src/main/java/com/vibhorkolte/contentcalendar/ContentCalendarApplication.java
@@ -11,8 +11,12 @@ public class ContentCalendarApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context =  SpringApplication.run(ContentCalendarApplication.class, args);
+		// Get all the beans
 //		context.getBeanDefinitionNames();
-		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+//		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		
+		// Get a single bean by name
+		System.out.println(context.getBean("contentCalendarApplication"));
 	}
 
 }
