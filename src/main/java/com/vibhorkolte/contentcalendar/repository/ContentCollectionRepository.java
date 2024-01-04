6949bc5c1f28bb5cc6578673be6beac39ee6c53b
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-01-04 23:08:19 +0530

    [DV-001] - initialize the repository using @PostConstruct

--- src/main/java/com/vibhorkolte/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/vibhorkolte/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,10 +1,15 @@
 package com.vibhorkolte.contentcalendar.repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import com.vibhorkolte.contentcalendar.model.Content;
+import com.vibhorkolte.contentcalendar.model.Status;
+import com.vibhorkolte.contentcalendar.model.Type;
+
+import jakarta.annotation.PostConstruct;
 
 import org.springframework.stereotype.Repository;
 
@@ -21,5 +26,15 @@ public class ContentCollectionRepository {
 	
 	public Optional<Content> findById(Integer id){
 		return content.stream().filter(v -> v.id().equals(id)).findFirst();	// findFirst() returns Optional<>
+	}
+	
+	// Tells spring to run this method after the dependency injection is done.
+	@PostConstruct
+	private void init() {
+		Content c = new Content(1, "First Blog Post", "First Blog Post", 
+				Status.IDEA, Type.ARTICLE, LocalDateTime.now(), null, "");
+		
+		content.add(c);
+		
 	}
 }
