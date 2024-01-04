004cd57e6ea5ffa6a9e75ea2689f4c86b8fa8ef3
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-01-04 22:04:06 +0530

    [DV-001] - Get all the beans in the IOC Container

--- src/main/java/com/vibhorkolte/contentcalendar/ContentCalendarApplication.java
+++ src/main/java/com/vibhorkolte/contentcalendar/ContentCalendarApplication.java
@@ -1,13 +1,18 @@
 package com.vibhorkolte.contentcalendar;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ContentCalendarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContentCalendarApplication.class, args);
+		ConfigurableApplicationContext context =  SpringApplication.run(ContentCalendarApplication.class, args);
+//		context.getBeanDefinitionNames();
+		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 }
